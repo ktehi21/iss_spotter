a48bd18892d5433b067bb3057a7abc45c2a67f01
tehi_kim <k.tehi21@gmail.com>
2023-03-21 14:25:37 +0000

    make 3 functions chaining into 1 function and exports it and call the function and catch the error message

---
+++ index2.js
@@ -0,0 +1,14 @@
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+// fetchMyIp()
+//   .then(fetchCoordsByIP)
+//   .then(fetchISSFlyOverTimes)
+//   .then(body => console.log(body));
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
